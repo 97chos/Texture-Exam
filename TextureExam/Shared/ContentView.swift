d5b712c2646f575f52d9d3a0d429977f9b58a2a6
Chosangho <97chos@naver.com>
2021-06-02 18:26:29 +0900

    프로젝트 생성 및 초기 AsyncDisplayKit import

---
+++ TextureExam/Shared/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 조상호 on 2021/06/02.
+//
+
+import SwiftUI
+import AsyncDisplayKit
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TextureExam/Shared/TextureExamApp.swift
@@ -0,0 +1,18 @@
+//
+//  TextureExamApp.swift
+//  Shared
+//
+//  Created by 조상호 on 2021/06/02.
+//
+
+import SwiftUI
+import AsyncDisplayKit
+
+@main
+struct TextureExamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
